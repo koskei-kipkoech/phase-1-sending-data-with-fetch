0c6f4f351919a8299a1f5ea23bccb69155bde8f0
Patrick Kipkoech <patrickwayy@gmail.com>
2024-10-09 09:18:27 +0300

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email){
+    console.log('dunction submitdt:',name,email)
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json",
+            'Accept':"application/json"
+        },
+        body:JSON.stringify({
+            name:name,
+            email:email
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log('completed',data);
+        console.log('id:',data.id)
+        const p = document.createElement('p')
+        p.textContent = `ID: ${data.id}`
+        document.body.appendChild(p);
+    })
+    .catch(error => {
+        console.log('Error', error);
+
+        const p = document.createElement('p');
+        p.textContent =`Error: ${error.message}`;
+        document.body.appendChild(p)
+    })
+}itch
